dcf91a4bcfe7d877b22b836864b8837d6db19b94
brijesh2 <brijesh09singh@gmail.com>
2023-07-23 23:49:15 +0530

    first commit

--- src/Problems/Union2Array.java
+++ src/Problems/Union2Array.java
@@ -1,4 +1,19 @@
 package Problems;
 
+import java.util.stream.IntStream;
+
 public class Union2Array {
+    public static void main(String[] args) {
+        int []a = {1,2,3};
+        int []b = {1,2,3,4,5};
+        int count = b.length;
+
+        for(int i = 0; i<a.length; i++){
+            if(!IntStream.of(b).anyMatch(v -> v == 1)){
+                count += 1;
+            }
+        }
+        System.out.println(count);
+
+    }
 }
